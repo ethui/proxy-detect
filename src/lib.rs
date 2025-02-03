ccf7ce7e99eb85be93a4c048c0f3194e1d417f20
Miguel Palhas <mpalhas@gmail.com>
2025-02-03 16:52:18 +0000

    Cleanup tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,15 +29,20 @@ where
 mod tests {
     use super::*;
     use alloy::{primitives::address, providers::ProviderBuilder, transports::http::reqwest::Url};
+    use lazy_static::lazy_static;
 
     const MAINNET_EIP1167_PROXY: Address = address!("0x6d5d9b6ec51c15f45bfa4c460502403351d5b999");
     const MAINNET_EIP1167_IMPL: Address = address!("0x210fF9Ced719E9bf2444DbC3670BAC99342126fA");
-    const MAINNET_RPC: &str = "https://eth.llamarpc.com";
+    lazy_static! {
+        static ref MAINNET_RPC: Url = Url::parse(
+            &std::env::var("ETH_MAINNET_RPC").unwrap_or("https://eth.rpc.blxrbdn.com".to_string())
+        )
+        .unwrap();
+    }
 
     #[tokio::test]
     async fn mainnet_eip1167() {
-        let url = Url::parse(MAINNET_RPC).unwrap();
-        let provider = ProviderBuilder::new().on_http(url);
+        let provider = ProviderBuilder::new().on_http(MAINNET_RPC.clone());
         let result = detect_proxy(MAINNET_EIP1167_PROXY, &provider)
             .await
             .unwrap();
